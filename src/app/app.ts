aba91de63d057ddccbe299a1a9d468f847fea071
Andrei kamarou <mr.akomarov@gmail.com>
2025-06-07 00:16:22 +0300

    added profile component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ProfileCard} from './common-ui/profile-card/profile-card';
+import {Profile} from './data/services/profile';
+import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    ProfileCard
+    ProfileCard,
+    JsonPipe
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'tik-talk';
+  protected title: string = 'tik-talk';
+
+  profileService: Profile = inject(Profile)
+  profiles: Object = []
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe(val =>  {
+     this.profiles = val
+    })
+  }
 }
---
+++ src/app/data/services/profile.ts
@@ -0,0 +1,16 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Profile {
+  http = inject(HttpClient)
+  constructor() { }
+
+  baseApiUrl = 'https://icherniakov.ru/yt-course/'
+
+  getTestAccounts() {
+    return this.http.get(`${this.baseApiUrl}account/test_accounts`)
+  }
+}
